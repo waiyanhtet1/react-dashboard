b82520f8e2b07fbb20aea74e39691432ce6860ab
Wai Yan Htet <waiyanhtet1661390@gmail.com>
2023-09-10 13:52:16 +0630

    feat: add sidebar ui

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,8 +9,13 @@ import { links } from "../data/dummy";
 function Sidebar() {
   const activeMenu = true;
 
+  const activeLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
+  const normalLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
+
   return (
-    <div className="ml-3 h-screen md:overflow-hidden md:hover:overflow-auto pb-10 ">
+    <div className="ml-3 h-screen md:overflow-hidden md:hover:overflow-auto pb-10">
       {activeMenu && (
         <>
           <div className="flex justify-between items-center">
@@ -21,10 +26,34 @@ function Sidebar() {
               <SiShopware /> <span>Shoppe</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
-              <button type="button">
+              <button
+                type="button"
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+              >
                 <MdOutlineCancel />
               </button>
             </TooltipComponent>
+          </div>
+          <div className="mt-10">
+            {links.map((item) => (
+              <div key={item.title}>
+                <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
+                  {item.title}
+                </p>
+                {item.links.map((link) => (
+                  <NavLink
+                    key={link.name}
+                    to={`/${link.name}`}
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
+                  >
+                    {link.icon}
+                    <span className="capitalize">{link.name}</span>
+                  </NavLink>
+                ))}
+              </div>
+            ))}
           </div>
         </>
       )}
